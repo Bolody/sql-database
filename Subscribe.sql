f7024989b0140a00b7b9e8cb756702bc32ccae0d
Bolody <112610614+Bolody@users.noreply.github.com>
2024-10-08 10:20:00 +0300

    Add files via upload

---
+++ Subscribe.sql
@@ -0,0 +1,18 @@
+create table Subscribe (
+	subscribe_id INT,
+	name TEXT,
+	description TEXT,
+	price INT,
+	count_of_cons INT,
+	worker_id INT
+);
+insert into Subscribe (subscribe_id, name, description, price, count_of_cons, worker_id) values (1, 'donec', 'Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet.', 47, 4, 70);
+insert into Subscribe (subscribe_id, name, description, price, count_of_cons, worker_id) values (2, 'nisl', 'Integer non velit.', 97, 8, 17);
+insert into Subscribe (subscribe_id, name, description, price, count_of_cons, worker_id) values (3, 'erat', 'Pellentesque ultrices mattis odio.', 22, 8, 60);
+insert into Subscribe (subscribe_id, name, description, price, count_of_cons, worker_id) values (4, 'eget', 'Suspendisse potenti.', 25, 1, 36);
+insert into Subscribe (subscribe_id, name, description, price, count_of_cons, worker_id) values (5, 'vitae', 'Etiam justo. Etiam pretium iaculis justo.', 54, 5, 22);
+insert into Subscribe (subscribe_id, name, description, price, count_of_cons, worker_id) values (6, 'elementum', 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', 72, 4, 22);
+insert into Subscribe (subscribe_id, name, description, price, count_of_cons, worker_id) values (7, 'ultrices', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', 53, 4, 45);
+insert into Subscribe (subscribe_id, name, description, price, count_of_cons, worker_id) values (8, 'semper', 'Duis mattis egestas metus. Aenean fermentum.', 49, 9, 66);
+insert into Subscribe (subscribe_id, name, description, price, count_of_cons, worker_id) values (9, 'velit', 'Maecenas pulvinar lobortis est.', 100, 3, 85);
+insert into Subscribe (subscribe_id, name, description, price, count_of_cons, worker_id) values (10, 'non', 'Vestibulum sed magna at nunc commodo placerat.', 72, 7, 41);
